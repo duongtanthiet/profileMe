b3559f54881f0574e36e40b723ab40db4af41a7d
duongtanthiet <dthiet213@gmail.com>
2023-07-30 15:40:27 +0700

    update

---
+++ asset/js/main.js
@@ -0,0 +1,47 @@
+ 
+ // icon navbar
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+menuIcon.onclick = function () {
+    menuIcon.classList.toggle('fa-x')
+    navbar.classList.toggle('active')
+}
+ // scroll section active link
+    let sections = document.querySelectorAll('section')
+    let navLinks = document.querySelectorAll('header nav a')
+    window.onscroll = function(){
+        sections.forEach(function(sec){
+            let top = window.scrollY
+            let offset = sec.offsetTop - 150
+            let height = sec.offsetHeight 
+            let id = sec.getAttribute('id') 
+            if(top >= offset & top < offset + height){
+                navLinks.forEach(function(link){
+                    link.classList.remove('active')
+                    document.querySelector('header nav a[href*= '+ id +']').classList.add('active')
+                })
+            }
+        })
+        // remove icon and navbar click link
+        menuIcon.classList.remove('fa-x')
+        navbar.classList.remove('active')
+    }
+    // scroll reveal
+    ScrollReveal({ 
+        reset : true ,
+        distance : '80px' ,
+        duration :  2000,
+        delay: 150
+     });
+     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+     ScrollReveal().reveal('.home-img, .services-container , .protfolio-box , .contact form', { origin: 'bottom' });
+     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    //  typed js
+    const typed = new Typed('.multiple-text',{
+        strings: ['Fontend Developer','Gamer' , 'Blogger'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay:1000,
+        loop: true
+    })
+
